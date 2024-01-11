7f75b07701d7b93ba5569641e06b755dc4f845e8
lucapolzone <lucapolzone@gmail.com>
2024-01-11 18:52:02 +0100

    feat: add boxes to for loop

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
+const wrapperBox = document.getElementById('wrapper-box');
+
+// const fizz = document.getElementById('fizz');
+// const buzz = document.getElementById('buzz');
+// const fizzbuzz = document.getElementById('fizzbuzz');
+
+let print = '';
+
+
 for (let i = 1; i <= 100; i++) {
 
   if (i % 3 === 0 && i % 5 === 0) {
       console.log('Fizzbuzz');
+      print = 'Fizzbuzz';
     } else if (i % 5 === 0) {
       console.log('Buzz');
+      print = 'Buzz';
     } else if (i % 3 === 0) {
       console.log('Fizz');
+      print = 'Fizz';
     } else {
       console.log(i);
+      print = i;
     } 
 
+    const box = 
+    `<div class="box-style ${print}-style">
+      ${print}
+    </div>`;
+
+    wrapperBox.innerHTML += box;
 };
